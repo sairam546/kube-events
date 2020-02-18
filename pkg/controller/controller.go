7191e607bcfa2fef91fc8d3f81e24950fb148821
sairam546 <sairam546@gmail.com>
2020-02-18 15:19:12 +0530

    Creating controller dynamically

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	"github.com/sairam546/kube-events/pkg/utils"
+	"github.com/Sirupsen/logrus"
 
 
 	"k8s.io/client-go/kubernetes"
@@ -12,7 +13,24 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	api_v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/util/workqueue"
 )
+
+// Event indicate the informerEvent
+type Event struct {
+	key          string
+	eventType    string
+	namespace    string
+	resourceType string
+}
+
+// Controller object
+type Controller struct {
+	logger       *logrus.Entry
+	clientset    kubernetes.Interface
+	queue        workqueue.RateLimitingInterface
+	informer     cache.SharedIndexInformer
+}
 
 func Start() {
 	fmt.Println("in controller.go Start")
@@ -43,5 +61,54 @@ func Start() {
 		)
 
 		fmt.Println(informer)
+		c := newResourceController(kubeClient, informer, "pod")
+		fmt.Println(c)
+		//stopCh := make(chan struct{})
+		//defer close(stopCh)
+
+		//go c.Run(stopCh)
+	}
+}
+
+func newResourceController(client kubernetes.Interface, informer cache.SharedIndexInformer, resourceType string) *Controller {
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	var newEvent Event
+	var err error
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			newEvent.key, err = cache.MetaNamespaceKeyFunc(obj)
+			newEvent.eventType = "create"
+			newEvent.resourceType = resourceType
+			logrus.WithField("pkg", "kubewatch-"+resourceType).Infof("Processing add to %v: %s", resourceType, newEvent.key)
+			if err == nil {
+				//queue.Add(newEvent)
+			}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			newEvent.key, err = cache.MetaNamespaceKeyFunc(old)
+			newEvent.eventType = "update"
+			newEvent.resourceType = resourceType
+			logrus.WithField("pkg", "kubewatch-"+resourceType).Infof("Processing update to %v: %s", resourceType, newEvent.key)
+			if err == nil {
+				//queue.Add(newEvent)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			newEvent.key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			newEvent.eventType = "delete"
+			newEvent.resourceType = resourceType
+			newEvent.namespace = utils.GetObjectMetaData(obj).Namespace
+			logrus.WithField("pkg", "kubewatch-"+resourceType).Infof("Processing delete to %v: %s", resourceType, newEvent.key)
+			if err == nil {
+				//queue.Add(newEvent)
+			}
+		},
+	})
+
+	return &Controller{
+		logger:       logrus.WithField("pkg", "kubewatch-"+resourceType),
+		clientset:    client,
+		informer:     informer,
+		queue:        queue,
 	}
 }
