58204923de7dcc20a287f2806ecaea21935654db
sairam546 <sairam546@gmail.com>
2020-02-18 13:33:41 +0530

    Kubernetes client creation - Adding utils and kept client creation functions there

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -2,8 +2,23 @@ package controller
 
 import (
 	"fmt"
+	"github.com/sairam546/kube-events/pkg/utils"
+
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func Start() {
 	fmt.Println("in controller.go Start")
+
+	var kubeClient kubernetes.Interface
+	_, err := rest.InClusterConfig()
+	if err != nil {
+		kubeClient = utils.GetClientOutOfCluster()
+	} else {
+		kubeClient = utils.GetClient()
+	}
+
+	fmt.Println(kubeClient)
 }
